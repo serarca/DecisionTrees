a352e473ca134b11389b8cccc8871ba3c788dd02
Sergio Camelo Gomez <serarcago@gmail.com>
2017-04-28 11:43:50 -0700

    added old split function

--- split.py
+++ split.py
@@ -17,7 +17,7 @@ def find_split(pData, nData, probSens = .0001):
 	while((posInd+1) < posLength and posVal == pData[posInd + 1]):
 		posInd += 1
 	while((negInd+1) < negLength and negVal == nData[negInd + 1]):
-		negInd += 1	
+		negInd += 1
 	val = 0
 	while( True ):
 		if (posVal < negVal):
@@ -30,7 +30,7 @@ def find_split(pData, nData, probSens = .0001):
 			if (negInd + 1 < negLength):
 				negVal = nData[negInd + 1]
 			negInd += 1
-			
+
 		else:
 			val = negVal
 			if (negInd + 1 < negLength):
@@ -58,3 +58,47 @@ def find_split(pData, nData, probSens = .0001):
 			break
 	return entSplit, entropy
 
+
+def find_split_old(posSort, negSort):
+	entMin = 99999999999
+	entSplit = -1
+
+	posCount = 0
+	negCount = 0
+
+	val = min(posSort[0], negSort[0])
+
+	posLength = len(pData)
+	negLength = len(nData)
+
+	while(True):
+		a = sum(i <= val for i in posSort)
+		b = sum(i <= val for i in negSort)
+
+		# print a, b
+		tot = max(a + b,0.1)
+		rest = max(posLength + negLength - a - b,0.1)
+		p1 = float(a)/float(tot)
+		p2 = float(posLength - a) / float(rest)
+
+		p1 = max(0.0001, min(p1, 0.9999))
+		p2 = max(0.0001, min(p2, 0.9999))
+
+		entropy = tot* (-p1*math.log(p1) - (1-p1) * math.log(1-p1)) + rest* (-p2*math.log(p2) - (1-p2) * math.log(1-p2))
+
+		if (entropy < entMin):
+			entMin = entropy
+			entSplit = val
+
+		if (math.isnan(val)):
+			break
+		elif (a < posLength and b < negLength):
+			val = min(posSort[a], negSort[b])
+		elif(a < posLength):
+			val = posSort[a]
+		elif(b < negLength):
+			val = negSort[b]
+		else:
+			break
+
+	return entMin, entSplit
