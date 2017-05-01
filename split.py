30ea6d99f019b3af06de83b6a1c80d035ac5117e
Sergio Camelo Gomez <serarcago@gmail.com>
2017-04-30 21:36:42 -0700

    debugging split

--- split.py
+++ split.py
@@ -1,7 +1,5 @@
 import math
-
-#nData = [0,0,0,0,1,2,3,3,4,4,5,6,6,7,7,7,7]
-#pData = [2,4,4,5,5,6,6,6,7,7,8,8,8,8,8,8,8,8]
+import random
 
 def find_split(pData, nData, probSens = .0001):
 	entMin = float('inf')
@@ -9,54 +7,103 @@ def find_split(pData, nData, probSens = .0001):
 	negLength = len(nData)
 	posInd = 0
 	negInd = 0
-	posCount = 0
-	negCount = 0
 	posVal = pData[posInd]
 	negVal = nData[negInd]
-	# Set the indices at the last index with the same value
+
 	while((posInd+1) < posLength and posVal == pData[posInd + 1]):
 		posInd += 1
 	while((negInd+1) < negLength and negVal == nData[negInd + 1]):
 		negInd += 1
-	val = 0
-	while( True ):
-		if (posVal < negVal):
-			val = posVal
-			if (posInd + 1 < posLength):
-				posVal = pData[posInd + 1]
-			posInd += 1
-		elif (posVal > negVal):
-			val = negVal
-			if (negInd + 1 < negLength):
-				negVal = nData[negInd + 1]
-			negInd += 1
 
-		else:
-			val = negVal
-			if (negInd + 1 < negLength):
-				negVal = nData[negInd + 1]
-			if (posInd + 1 < posLength):
-				posVal = pData[posInd + 1]
-			posInd += 1
-			negInd += 1
-		total = max(posInd + negInd, .1)
-		rest = max(posLength + negLength - posInd - negInd, .1)
-		p1 = float(posInd)/float(total)
-		p2 = float(posLength - posInd)/float(rest)
+	if (posInd == 0 and posVal > negVal):
+		posInd = -1
+		negInd = 0
+	elif (negInd == 0 and posVal < negVal):
+		posInd = 0
+		negInd = -1
+
+	# Set the indices at the last index with the same value
+
+	val = min(posVal,negVal)
+	i=0
+
+	while( True ):
+
+		posCount = posInd + 1
+		negCount = negInd + 1
+		total = max(posCount + negCount, .1)
+		rest = max(posLength + negLength - posCount - negCount, .1)
+		p1 = float(posCount)/float(total)
+		p2 = float(posLength - posCount)/float(rest)
 		p1 = max(probSens, min(p1, 1 - probSens))
 		p2 = max(probSens, min(p2, 1 - probSens))
 		entropy =  (total * (-p1 * math.log(p1) - (1 - p1) * math.log(1 - p1)) +
 		 			rest * (-p2 * math.log(p2) - (1 - p2) * math.log(1 - p2)))
+
 		if (entropy < entMin):
 			entMin = entropy
 			entSplit = val
-		while((posInd+1) < posLength and posVal == pData[posInd+1]):
-			posInd += 1
-		while((negInd+1) < negLength and negVal == nData[negInd+1]):
-			negInd += 1
-		if(posInd == posLength or  negInd == negLength):
+
+		if (posInd == posLength - 1):
 			break
-	return entSplit, entropy
+		else:
+			posNext = pData[posInd+1]
+		if (negInd == negLength - 1):
+			break
+		else:
+			negNext = nData[negInd+1]
+
+		if (posVal == negVal):
+			if (posNext == negNext):
+				posVal = posNext
+				negVal = negNext
+				val = posVal
+				posInd += 1
+				negInd += 1
+				while((posInd+1) < posLength and posVal == pData[posInd+1]):
+					posInd += 1
+				while((negInd+1) < negLength and negVal == nData[negInd+1]):
+					negInd += 1
+			elif (posNext < negNext):
+				posVal = posNext
+				val = posVal
+				posInd += 1
+				while((posInd+1) < posLength and posVal == pData[posInd+1]):
+					posInd += 1
+			elif (posNext > negNext):
+				negVal = negNext
+				val = negVal
+				negInd += 1
+				while((negInd+1) < negLength and negVal == nData[negInd+1]):
+					negInd += 1
+		else:
+			if (posNext < negNext):
+				posVal = posNext
+				val = posVal
+				posInd += 1
+				while((posInd+1) < posLength and posVal == pData[posInd+1]):
+					posInd += 1
+			elif (posNext > negNext):
+				negVal = negNext
+				val = negVal
+				negInd += 1
+				while((negInd+1) < negLength and negVal == nData[negInd+1]):
+					negInd += 1
+			else:
+				posVal = posNext
+				negVal = negNext
+				val = posVal
+				posInd += 1
+				negInd += 1
+				while((posInd+1) < posLength and posVal == pData[posInd+1]):
+					posInd += 1
+				while((negInd+1) < negLength and negVal == nData[negInd+1]):
+					negInd += 1
+
+
+
+
+	return entSplit, entMin
 
 
 def find_split_old(posSort, negSort):
@@ -68,8 +115,8 @@ def find_split_old(posSort, negSort):
 
 	val = min(posSort[0], negSort[0])
 
-	posLength = len(pData)
-	negLength = len(nData)
+	posLength = len(posSort)
+	negLength = len(negSort)
 
 	while(True):
 		a = sum(i <= val for i in posSort)
@@ -101,4 +148,4 @@ def find_split_old(posSort, negSort):
 		else:
 			break
 
-	return entMin, entSplit
+	return entSplit, entMin
