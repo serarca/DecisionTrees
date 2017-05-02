20d9635ee57818e7ef095b7b45105a4584e60cb7
Sergio Camelo Gomez <serarcago@gmail.com>
2017-05-01 18:58:57 -0700

    added search_split

--- split.py
+++ split.py
@@ -1,5 +1,51 @@
 import math
 import random
+import pandas as pd
+import numpy as np
+
+var = ["a","b","c"]
+pData = [random.randint(0,10) for i in range(0,10)]
+pData.sort()
+nData = [random.randint(0,10) for i in range(0,10)]
+nData.sort()
+
+a = [random.randint(0,10) for i in range(0,10)]
+b = [random.randint(0,10) for i in range(0,10)]
+c = [random.randint(0,10) for i in range(0,10)]
+posData = pd.DataFrame.from_items([("a",a), ("b",b), ("c",c)])
+
+a = [random.randint(0,10) for i in range(0,10)]
+b = [random.randint(0,10) for i in range(0,10)]
+c = [random.randint(0,10) for i in range(0,10)]
+negData = pd.DataFrame.from_items([("a",a), ("b",b), ("c",c)])
+
+posOrder = {v:np.argsort(posData[v].values) for v in var}
+negOrder = {v:np.argsort(negData[v].values) for v in var}
+
+# Receives a set of indices and a set of variables
+def search_split(pIndex, nIndex, pOrder, nOrder, var):
+	entMin = float('inf')
+	for v in var:
+		pData = (posData[v][pIndex].values)[pOrder[v]]
+		nData = (negData[v][nIndex].values)[nOrder[v]]
+		split, ent, pInd, nInd = find_split(pData, nData)
+		if (ent < entMin):
+			varMin = v
+			entMin = ent
+			splMin = split
+			nIndMin = nInd
+			pIndMin = pInd
+	left = [pIndex[0:(pIndMin + 1)], nIndex[0:(nIndMin + 1)]]
+	right = [pIndex[(pIndMin + 1):], nIndex[(nIndMin + 1):]]
+	return varMin, splMin, left, right
+
+
+
+
+
+
+
+
 
 def find_split(pData, nData, probSens = .0001):
 	entMin = float('inf')
@@ -43,6 +89,8 @@ def find_split(pData, nData, probSens = .0001):
 		if (entropy < entMin):
 			entMin = entropy
 			entSplit = val
+			pIndMin = posInd
+			nIndMin = negInd
 
 		if (posInd == posLength - 1):
 			break
@@ -100,52 +148,4 @@ def find_split(pData, nData, probSens = .0001):
 				while((negInd+1) < negLength and negVal == nData[negInd+1]):
 					negInd += 1
 
-
-
-
-	return entSplit, entMin
-
-
-def find_split_old(posSort, negSort):
-	entMin = 99999999999
-	entSplit = -1
-
-	posCount = 0
-	negCount = 0
-
-	val = min(posSort[0], negSort[0])
-
-	posLength = len(posSort)
-	negLength = len(negSort)
-
-	while(True):
-		a = sum(i <= val for i in posSort)
-		b = sum(i <= val for i in negSort)
-
-		# print a, b
-		tot = max(a + b,0.1)
-		rest = max(posLength + negLength - a - b,0.1)
-		p1 = float(a)/float(tot)
-		p2 = float(posLength - a) / float(rest)
-
-		p1 = max(0.0001, min(p1, 0.9999))
-		p2 = max(0.0001, min(p2, 0.9999))
-
-		entropy = tot* (-p1*math.log(p1) - (1-p1) * math.log(1-p1)) + rest* (-p2*math.log(p2) - (1-p2) * math.log(1-p2))
-
-		if (entropy < entMin):
-			entMin = entropy
-			entSplit = val
-
-		if (math.isnan(val)):
-			break
-		elif (a < posLength and b < negLength):
-			val = min(posSort[a], negSort[b])
-		elif(a < posLength):
-			val = posSort[a]
-		elif(b < negLength):
-			val = negSort[b]
-		else:
-			break
-
-	return entSplit, entMin
+	return entSplit, entMin, pIndMin, nIndMin
