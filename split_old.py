7946597ce73f410885d553ac4f907df3a6577ec3
Sergio Camelo Gomez <serarcago@gmail.com>
2017-05-04 11:01:32 -0700

    debugging

--- split_old.py
+++ split_old.py
@@ -17,7 +17,7 @@ def find_split_old(posSort, negSort):
 		p1 = max(0.0001, min(p1, 0.9999))
 		p2 = max(0.0001, min(p2, 0.9999))
 		entropy = tot* (-p1*math.log(p1) - (1-p1) * math.log(1-p1)) + rest* (-p2*math.log(p2) - (1-p2) * math.log(1-p2))
-		print val, entropy
+		#print val, entropy
 		if (entropy < entMin):
 			entMin = entropy
 			entSplit = val
