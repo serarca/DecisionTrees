3ad0bce47acd95031225af264ff551820c07a94a
Sergio Camelo Gomez <serarcago@gmail.com>
2017-05-11 16:03:30 -0700

    random_script

--- Scripts/random_script.py
+++ Scripts/random_script.py
@@ -15,18 +15,18 @@ negData = negData.reshape(18710, 70208)
 negData = pd.DataFrame(negData)
 
 
-trainData = np.fromfile("/lfs/local/camelo/DecisionTrees/trainData.np")
-trainData = trainData.reshape(4779, 70208)
-trainData = pd.DataFrame(trainData)
+testData = np.fromfile("/lfs/local/camelo/DecisionTrees/trainData.np")
+testData = trainData.reshape(4779, 70208)
+testData = pd.DataFrame(trainData)
 
 true = np.fromfile("/lfs/local/camelo/DecisionTrees/true.np")
-variables = list(posData.columns.values)
+variables = np.array(list(posData.columns.values))
 weights = linear_weights(variables, 0.35)
 # Sample a square root size of the number of variables
-sample = random.sample(range(0,len(variables)), int(np.floor(len(variables)**0.5))
+sample = random.sample(range(0,len(variables)), int(np.floor(len(variables)**0.5)))
 
 
 start = time.time()
-tree = fit_tree(posData, negData, variables, 3, random.sa)
+tree = fit_tree(posData, negData, variables[sample], 3, weights)
 end = time.time()
 print(end - start)
